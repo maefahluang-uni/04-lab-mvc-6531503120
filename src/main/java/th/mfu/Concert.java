da9b9f3c6941feda249b279c077880129aa4e711
6531503120 <6531503120@lamduan.mfu.ac.th>
2023-09-12 22:50:37 +0700

    GG

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,40 +4,39 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
-
+    // TODO: add attributes
     private int id;
     private String title;
     private String performer;
-    private String date;
+    private Date date;
     private String description;
 
     public Concert() {
-        id = 0;
-        title = "";
-        performer = "";
-        date = "";
-        description = "";
     }
+
     public Concert(String title, String description) {
-        //TODO: set attributes
-        this();
+        // TODO: set attributes
         this.title = title;
         this.description = description;
     }
- 
-    //TODO: add getters and setters for all attributes
 
+    public String getTitle() {
+        // TODO: return title
+        return title;
+    }
+
+    public String getDescription() {
+        // TODO: return description
+        return description;
+    }
+
+    // TODO: add getters and setters
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public void setTitle(String title) {
@@ -52,25 +51,15 @@ public class Concert {
         this.performer = performer;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(Date date) {
         this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    // You can use Source action/generate getter setter function
-    @Override
-    public String toString() {
-        return "Concert [id=" + id + ", title=" + title + ", performer=" + performer + ", date=" + date + ", description=" + description + "]";
     }
 }
